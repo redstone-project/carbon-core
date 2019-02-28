63b4655df10807342e41ff7387157d9705d65f14
lightless233 <lightless@foxmail.com>
2019-02-28 21:54:36 +0800

    [UPDATE] add engines.

---
+++ carbon/core/engines/disassemble.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    carbon.core.engines.disassemble
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Convert job to task
+
+    :author:    lightless <root@lightless.me>
+    :homepage:  None
+    :license:   GPL-3.0, see LICENSE for more details.
+    :copyright: Copyright (c) 2017-2019 lightless. All rights reserved
+"""
+
+
